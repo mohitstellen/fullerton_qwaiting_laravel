b1f7c499a32f4ef3a5b77265738a70044f38f7cb
ManpreetS0029 <manpreetmatharoo2@gmail.com>
2025-12-24 18:29:55 +0530

    Added missing fields and functionalities and fixed bugs and issues with ui and functionality

--- public/js/livewire.js
+++
@@ -1,8 +0,0 @@
-// Custom Livewire extensions
-// This file can be used for custom Livewire JavaScript if needed
-// Livewire core functionality is already included via @livewireScripts
-
-document.addEventListener('livewire:init', () => {
-    // Add any custom Livewire initialization here if needed
-});
-
