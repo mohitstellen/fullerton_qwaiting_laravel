bab3e4844d971b44af5b618ddc4b041b83c629c1
manpreet-stellen <manpreet@stelleninfotech.in>
2026-01-27 16:28:36 +0530

    Fixed issues in login and signup and made a new ui of permissions module and fixed some bugs and issues

--- resources/views/livewire/roles-manager.blade.php
+++ resources/views/livewire/roles-manager.blade.php
@@ -92,6 +92,7 @@
         </div>
     </div>
 </div>
+
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
     document.addEventListener("DOMContentLoaded", function() {
